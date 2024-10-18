2ef7e47bc9d0583663052faed7c689e21aeb271c
JanClusmann <82999332+JanClusmann@users.noreply.github.com>
2024-10-18 16:41:47 +0200

    Create Prompts.py

---
+++ Prompts.py
@@ -0,0 +1,6 @@
+Analyse this whole-slide pathology Image of a lymph node dissection of a Patient with breast Cancer. Focus on the Lymph node status. Give your answer as X/Y, depending on "X" the number of tumor-positive lymph nodes and "Y" the absolute number of visible lymph nodes. Strictly adhere to this Format for your answer: {X/Y}. "
+
+
+Analyse this whole-slide pathology Image of a patient with colorectal cancer. Focus on the T-Stage as in TNM classification. Give your answer as pT{X}.
+
+Molecular status: "Analyse this whole-slide pathology Image of a Patient with thyroid cancer. What is the likely mutational status? BRAF MUT? RAS MUT? Wildtype? Give your answer as 
